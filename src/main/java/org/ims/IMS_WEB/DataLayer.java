be256e9abebf5bad16cfa777b9a83119cd6069cd
AlexHajek <alexanderjhajek@gmail.com>
2016-08-19 11:25:02 -0400

    Working Again, Maybe

--- src/main/java/org/ims/IMS_WEB/DataLayer.java
+++ src/main/java/org/ims/IMS_WEB/DataLayer.java
@@ -1,6 +1,9 @@
 package org.ims.IMS_WEB;
 
+import java.util.List;
+
 import org.hibernate.Session;
+import org.ims.beans.StateAbbrvBean;
 
 public class DataLayer {
 	private Session session;//Session-per-request
@@ -13,5 +16,11 @@ public class DataLayer {
 	
 	public void close(){
 		if(session!=null) session.close();
+	}
+	
+	public int test(){return 68;}
+	
+	public List<StateAbbrvBean> getAllAbbr(){
+		return dao.getAllStatesAbb();
 	}
 }
